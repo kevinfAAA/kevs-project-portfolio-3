9100cd70617f3b225b2c09da80c6a3a29461100f
kevinfAAA <kevin.foley21@hotmail.com>
2021-08-08 12:22:51 +0000

    Import modules and add extract function

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-# python code goes here
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract(page):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+    url = f'https://ie.indeed.com/jobs?q=python&l=dublin&start={page}'
+    r = requests.get(url, headers)
+    return r.status_code
+
+print(extract(0))
