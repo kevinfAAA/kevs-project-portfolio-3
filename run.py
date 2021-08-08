b7c96b430b4cdd2b882e6d572d55a6c58d135c04
kevinfAAA <kevin.foley21@hotmail.com>
2021-08-08 13:25:10 +0000

    Scrapped the first page of indeeds website and outputted a list of dictionaries with the title, company, salary and summary

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
+# import modules
 import requests
 from bs4 import BeautifulSoup
 
 
+# Extrct Function to Scrape the website
 def extract(page):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
     url = f'https://ie.indeed.com/jobs?q=python&l=dublin&start={page}'
+    # pass the url into extract function
     r = requests.get(url, headers)
     content = BeautifulSoup(r.content, 'html.parser')
+    # return indeed webiste content
     return content
 
 
+# filter through indeed using find_all function to find certain tags
 def transform(content):
     divs = content.find_all('div', class_='slider_container')
     for item in divs:
@@ -20,11 +25,20 @@ def transform(content):
         except:
             salary = ''
         summary = item.find('div', class_='job-snippet').text.strip()
-        print(summary)
+
+# Store content into a dictionary and append to a list
+        job = {
+            'title': title,
+            'company': company,
+            'salary': salary,
+            'summary': summary
+        }
+        joblist.append(job)
     return
 
 
+joblist = []
 content = extract(0)
 transform(content)
-
+print(joblist)
 
