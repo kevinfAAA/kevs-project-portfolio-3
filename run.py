72319cb6b9c2e4e5804dbb9af0ab9457bc5d2e98
kevinfAAA <kevin.foley21@hotmail.com>
2021-08-09 18:16:57 +0000

    Add installs to requirements file and added job_type and location user inputs

--- run.py
+++ run.py
@@ -3,13 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+joblist = []
+
 
 # Extrct Function to Scrape the website
 def extract(page):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
-    url = f'https://ie.indeed.com/jobs?q=python&l=dublin&start={page}'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+        }
+    url = f'https://ie.indeed.com/jobs?q={job_type}&l={location}&start={page}'
     # pass the url into extract function
     r = requests.get(url, headers)
+    if not r.status_code == 200:
+        raise Exception('No content returning from from url.')
     content = BeautifulSoup(r.content, 'html.parser')
     # return indeed webiste content
     return content
@@ -17,13 +23,15 @@ def extract(page):
 
 # filter through indeed using find_all function to find certain tags
 def transform(content):
+    """The transform function is used to
+    find specific tags within the html parsed."""
     divs = content.find_all('div', class_='slider_container')
     for item in divs:
         title = item.find('h2').text.strip()
         company = item.find('span', class_='companyName').text.strip()
         try:
             salary = item.find('span', class_='salary-snippet').text.strip()
-        except:
+        except Exception as ex:
             salary = ''
         summary = item.find('div', class_='job-snippet').text.strip()
 
@@ -35,10 +43,17 @@ def transform(content):
             'summary': summary
         }
         joblist.append(job)
-    return
+    return None
 
 
-joblist = []
+if __name__ == '__main__':
+    print('what job are you looking for?')
+    job_type = input('')
+
+    print('What location are you looking for?')
+    location = input('')
+
+
 # For loop to loop through the first three pages in a step of ten
 for i in range(0, 40, 10):
     print(f'Getting page {i} ')
