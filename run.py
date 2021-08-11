591d2791ee793691a436832bb8cdb9da60182841
kevinfAAA <kevin.foley21@hotmail.com>
2021-08-11 14:00:07 +0000

    Add update to requirements file

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 joblist = []
 
 
-# Extrct Function to Scrape the website
+# Extract Function to Scrape the website
 def extract(page):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
